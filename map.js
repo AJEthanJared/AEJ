54db197a4c3815b074a3dfb4faef80805ab788b3
yxl8 <yxl8@txstate.edu>
2025-10-10 18:55:08 -0500

    Upload for further editing.

--- map.js
+++ map.js
@@ -6,3 +6,24 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
 }).addTo(map);
+
+
+var polygon = turf.polygon([
+  [
+    [125, -15],
+    [113, -22],
+    [154, -27],
+    [144, -15],
+    [125, -15],
+  ],
+]);
+
+var area = turf.area(polygon);
+L.geoJSON(polygon).addTo(map);
+
+var testmarker = L.marker([-15, 125]).addTo(map);
+testmarker.bindPopup(area + "m^2").openPopup();
+testmarker.on('click', function (e) { map.setView(e.latlng, 14); });
+
+let polyLayer = L.geoJSON(polygon).addTo(map);
+polyLayer.bindPopup("Test: " + area);
